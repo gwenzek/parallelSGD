4ac0f3e0a6fc819f14b99b953d4effc66ad0300b
Guillaume Wenzek <guillaume.wenzek@polytechnique.org>
2014-04-29 10:39:40 +0200

    creating a working thread class

--- permThread.py
+++ permThread.py
@@ -2,7 +2,8 @@ from multiprocessing import Process, Value, Array
 
 
 class permThread (Process):
-    def __init__(self, event, boolArray, index, workingThreads, nRows, nCols):
+    def __init__(self, event, boolArray, index, workingThreads,
+                 nRows, nCols, maxIter):
         Process.__init__(self)
         self.event = event.Value
         self.workingThreads = workingThreads
@@ -11,25 +12,33 @@ class permThread (Process):
         self.nThread = len(workingThreads)
         self.boolArray = boolArray
         self.index = index
+        self.maxIter = maxIter
+        self.nIter = 0
 
     def run(self):
         print "Starting permThread"
-        self.createOneShuffle()
-        self.boolArray[self.index] = True
-        if checkArray(self.boolArray):
-            self.event.set()
-            self.event.clear()
+        while self.hasNext():
+            self.createOneShuffle()
+            if self.checkArray():
+                self.event.set()
+                self.event.clear()
+            self.nIter += 1
         print "Exiting permThread"
 
-    def checkArray(boolArray):
-        for b in boolArray:
+    def checkArray(self):
+        self.boolArray[self.index] = True
+        for b in self.boolArray:
             if not b:
                 return False
         """Every one finished"""
-        for i in range(1+nThread):
-            boolArray[i] = False
+        for i in range(1 + self.nThread):
+            self.boolArray[i] = False
+        print "!!! Perm is the last to finish !!!"
         return True
-
 
     def createOneShuffle(self):
         print "shuffling"
+
+    def hasNext(self):
+        return self.nIter >= self.maxIter
+
