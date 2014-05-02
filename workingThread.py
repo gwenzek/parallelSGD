a6de8a8dbe9351b4c2ebf43f306f0c62fa12fde2
Guillaume Wenzek <guillaume.wenzek@polytechnique.org>
2014-05-02 11:49:17 +0200

    preprocessing plus Matthieu version

--- workingThread.py
+++ workingThread.py
@@ -1,5 +1,4 @@
 from multiprocessing import Process
-from time import sleep
 
 
 def printMatrix(matrix, nRows, nCols):
@@ -14,60 +13,91 @@ def printMatrix(matrix, nRows, nCols):
 class WorkingThread(Process):
 
     def __init__(self,
-                 event, counter, lock,
-                 index, nThread, dim,
+                 eventFRold,eventFRnew,eventFEold,eventFEnew, 
+                 counterFR,counterFE, 
+                 lockFR,lockFE, index, nWThread, dim,
                  sharedBuffer, bufferSize,
-                 maxIter, globalMatrix):
+                 nbEpochs,nwThread,
+            L,R,mij,om_i,om_j,
+            mu,alpha,dalpha,r
+                 ):
         Process.__init__(self)
-        self.event = event
-        self.counter = counter
-        self.lock = lock
-        #self.boolArray = boolArray
+        
+        # Concurrency tools
+        self.eventFRold = eventFRold
+        self.eventFEold = eventFEold
+        self.eventFRnew = eventFRnew
+        self.eventFEnew = eventFEnew
+        self.counterFR = counterFR
+        self.counterFE = counterFE
+        self.lockFR = lockFR
+        self.lockFE = lockFE
+        
+        # Process-specific data
         self.index = index
-
         self.buffer = sharedBuffer
         self.bufferRead = 0
         self.bufferSize = bufferSize
-
+        
+        # Problem-specific data
         self.nRows, self.nCols = dim
-
-        self.maxIter = maxIter
-        self.nIter = 0
-        self.nThread = nThread
-        self.globalMatrix = globalMatrix
+        self.nbEpochs = nbEpochs
+        self.nWThread = nWThread
+        self.L = L
+        self.R = R
+        self.mij = mij
+        self.om_i = om_i
+        self.om_j = om_j
+        self.mu = mu
+        self.alpha = alpha
+        self.dalpha = dalpha
+        self.r = r
 
     def run(self):
         print "Starting Thread %d " % self.index
 
-        while self.hasNext():
-            self.treatOneRound()
-            if self.checkArray():
-                print "Thread %d finished last" % self.index
-                #printMatrix(self.globalMatrix, self.nRows, self.nCols)
-                self.event.set()
-                sleep(2)
-                self.event.clear()
+        for _ in range(self.nbEpochs):
+            for _ in range(self.nWThread):
+                self.treatOneRound()
+                if self.checkArray(self.lockFR,self.counterFR,self.eventFRold,self.nWThread):
+                    print "Thread %d finished last" % self.index
+                    self.eventFRnew.set()
 
+                else:
+                    print "Thread %d waiting..." % self.index
+                    self.eventFRnew.wait()
+                    print "Thread %d resuming, %d" \
+                        % (self.index, self.peek())
+                
+                self.eventFRnew,self.eventFRold=self.eventFRold,self.eventFRnew
+                
+            
+            if self.checkArray(self.lockFE,self.counterFE,self.nWThread+1):
+                print "Epoch : Thread %d finished last" % self.index
+                self.eventFEnew.set()
             else:
-                print "Thread %d waiting..." % self.index
-                self.event.wait()
+                print "Epoch : Thread %d waiting..." % self.index
+                self.eventFEnew.wait()
+                print "Epoch : Thread %d resuming, %d" \
+                        % (self.index, self.peek())
+            self.eventFEnew,self.eventFEold=self.eventFEold,self.eventFEnew
+            
 
-            print "Thread %d resuming, %d" \
-                % (self.index, self.peek())
-            self.nIter += 1
 
         print "Exiting Thread %d " % self.index
 
-    def checkArray(self):
-        self.lock.acquire()
-        self.counter.value += 1
-        if self.counter.value > self.nThread:
-            self.counter.value = 0
-            self.lock.release()
+    def checkArray(self,lock,counter,event,value):
+        lock.acquire()
+        counter.value += 1
+        if counter.value == value:
+            event.clear()
+            counter.value = 0
+            lock.release()
             return True
         else:
-            self.lock.release()
+            lock.release()
             return False
+        
         # self.boolArray[self.index] = True
         # for b in self.boolArray:
         #     if not b:
@@ -78,19 +108,33 @@ class WorkingThread(Process):
         # return True
 
     def treatOneRound(self):
-        print "Thread %d | Iter %d | queue %d" \
-            % (self.index, self.nIter, self.peek())
 
         n = self.read()
         for _ in range(n):
             i = self.read()
             j = self.read()
-            self.globalMatrix[self.nRows * i + j] = self.index + 1
+            if self.method == "NUCLEAR_NORM":
+                    fp = self.fprime((i,j),self.multiply(i,j))
+                    for ind in range(self.r):
+                        newL = (1-self.mu*self.alpha/self.om_i[i])*self.L[i*self.r+ind]-\
+                        self.alpha*fp*self.R[j*self.r+ind]
+                        newR = (1-self.mu*self.alpha/self.om_j[j])*self.R[j*self.r+ind]-\
+                        self.alpha*fp*self.L[i*self.r+ind]
+                        self.L[i*self.r+ind] = newL
+                        self.R[i*self.r+ind] = newR
             #print "Thread %d : (%d, %d)" % (self.index, i, j)
-
-        sleep(1)
+            
         #printMatrix(self.globalMatrix, self.nRows, self.nCols)
-
+    
+    def multiply(self,i,j):
+        res = 0
+        for ind in range(self.r):
+            res+= L[i*self.r+ind]*R[j*self.r+ind]
+        return res
+        
+    def fprime(ind,val):
+        return 2*(val-self.mij[ind])
+            
     def read(self):
         n = self.buffer[self.bufferRead]
         self.bufferRead = (self.bufferRead + 1) % self.bufferSize
@@ -109,5 +153,4 @@ class WorkingThread(Process):
         print "*"
         self.bufferRead = oldRead
 
-    def hasNext(self):
-        return self.nIter < self.maxIter
+
